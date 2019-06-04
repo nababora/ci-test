6f76f60885b8ecb14018ff87bb517bc5264fe615
nababora <nababora@naver.com>
2019-06-04 15:53:32 +0900

    really?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-console.log("Test Server Started");
+console.log("Teset Server Started");
 
 app.get('/',function(req,res) {
   res.send("Hedddddddddddddllo World\n");
